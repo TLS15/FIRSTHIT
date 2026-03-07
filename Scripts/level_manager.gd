a72719732abf8425a02e52360942bb48e9b703fc
Tamer Suliman <84022203+TLS15@users.noreply.github.com>
2026-03-07 21:45:48 +0100

    calc pop

--- Scripts/level_manager.gd
+++ Scripts/level_manager.gd
@@ -1,21 +1,29 @@
 extends Node2D
 
+const TICK_TIME := 1.0 / 5.0
+var tick_accumulator := 0.0
+
 @export var money: float = 0
 @export var population: int
 
+func _process(delta):
+	tick_accumulator += delta
+	
+	while tick_accumulator >= TICK_TIME:
+		tick()
+		tick_accumulator -= TICK_TIME
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-	
-func  _process(delta: float) -> void:
+func tick():
 	money += calc_revenue()
+	population += calc_reproduction()
 	$"../UIMaster/UILevel/HBoxContainer/Money".text = "Money: %.0f" % money
-	print(money)
-	
+	$"../UIMaster/UILevel/HBoxContainer/Population".text = "Population: " + str(population)
 	
 func calc_revenue() -> float:
-	return 1.0 / 60.0 # assuming  60 ticks per second
+	return 10.0 * TICK_TIME
+	
+func calc_reproduction() -> int:
+	return 1
 	
 func load_level(level: LevelResource):
 	money = level.starting_money
