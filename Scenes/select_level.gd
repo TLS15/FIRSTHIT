bd9ee0ebb5d6d3254cb219258dda066798e85cc1
Tamer Suliman <84022203+TLS15@users.noreply.github.com>
2026-03-09 10:51:09 +0100

    automatic level loading

--- Scenes/select_level.gd
+++ Scenes/select_level.gd
@@ -3,11 +3,18 @@ extends PopupMenu
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var dir = DirAccess.open("res://LevelData")
-	var i:int = 1
-	for file in dir:
+	#var dir = DirAccess.open("res://LevelData")
+	#var i:int = 1
+	#for file in dir:
+		#add_item()
+		#++i
+	var levels_available: int = ResourceLoader.list_directory("res://LevelData").size()
+	
+	var i: int = 0
+	while i < levels_available:
+		i += 1
 		add_item("Level" + str(i))
-		++i
+		print(i)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
